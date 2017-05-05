66332bbcb8d1c0c8ff90db55defe240e5ff0bb4c
Rodrigo Pina Lozano (CI/BDM-EU) <rodrigo.pina@de.bosch.com>
2017-05-05 09:35:31 +0200

    Added missing services

--- api/startup.sh
+++ api/startup.sh
@@ -11,15 +11,15 @@ java -jar $THINGBOOK_PARAMS product-taxonomy-*.jar >product-taxonomy.out 2>&1 &
 java -jar $THINGBOOK_PARAMS rating-*.jar >rating.out 2>&1 &
 java -jar $THINGBOOK_PARAMS prima-*.jar >prima.out 2>&1 &
 java -jar $THINGBOOK_PARAMS authentication-*.jar >authentication.out 2>&1 &
-#java -jar $THINGBOOK_PARAMS inventory-*.jar >inventory.out 2>&1 &
-#java -jar $THINGBOOK_PARAMS manufacturer-*.jar >manufacturer.out 2>&1 &
-#java -jar $THINGBOOK_PARAMS manufacturer-web-*.jar >manufacturer-web.out 2>&1 &
-#java -jar $THINGBOOK_PARAMS service-company-*.jar >service-company.out 2>&1 &
-#java -jar $THINGBOOK_PARAMS servicecompany-web-*.jar >servicecompany-web.out 2>&1 &
-#java -jar $THINGBOOK_PARAMS user-*.jar >user.out 2>&1 &
-#java -jar $THINGBOOK_PARAMS tb-common-*.jar >tb-common.out 2>&1 &
-#java -jar $THINGBOOK_PARAMS common-web-*.jar >common-web.out 2>&1 &
-#java -jar $THINGBOOK_PARAMS mobile-gateway-*.jar >mobile-gateway.out 2>&1 &
+java -jar $THINGBOOK_PARAMS inventory-*.jar >inventory.out 2>&1 &
+java -jar $THINGBOOK_PARAMS manufacturer-*.jar >manufacturer.out 2>&1 &
+java -jar $THINGBOOK_PARAMS manufacturer-web-*.jar >manufacturer-web.out 2>&1 &
+java -jar $THINGBOOK_PARAMS service-company-*.jar >service-company.out 2>&1 &
+java -jar $THINGBOOK_PARAMS servicecompany-web-*.jar >servicecompany-web.out 2>&1 &
+java -jar $THINGBOOK_PARAMS user-*.jar >user.out 2>&1 &
+java -jar $THINGBOOK_PARAMS tb-common-*.jar >tb-common.out 2>&1 &
+java -jar $THINGBOOK_PARAMS common-web-*.jar >common-web.out 2>&1 &
+java -jar $THINGBOOK_PARAMS mobile-gateway-*.jar >mobile-gateway.out 2>&1 &
 
 sleep 3
 mongorestore dump/
